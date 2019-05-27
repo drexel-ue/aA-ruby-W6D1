33f2efa6a75926be7f2f3fa98228281017ea968a
ikesh pack <packappsforyou@gmail.com>
2019-05-27 14:04:14 -0400

    added parent= method

--- intro_polytree/skeleton/lib/00_tree_node.rb
+++ intro_polytree/skeleton/lib/00_tree_node.rb
@@ -1,11 +1,18 @@
 class PolyTreeNode
 
-    attr_accessor :value, :parent, :children
+    attr_accessor :value, :children
+    attr_reader :parent
 
     def initialize(value)
         @value = value
         @parent = nil
         @children = []
+    end
+
+    def parent=(node = nil)
+        @parent.children.delete(self) if @parent != nil
+        @parent = node 
+        node.children << self if node != nil && !node.children.include?(self)
     end
 
 end
