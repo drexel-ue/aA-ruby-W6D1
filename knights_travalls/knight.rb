b6890e11009b2bef6ac8afde23845d0a40d7f6fd
ikesh pack <packappsforyou@gmail.com>
2019-05-27 19:16:21 -0400

    Pseudo-coded KightPathFinder#build_root_tree method

--- knights_travalls/knight.rb
+++ knights_travalls/knight.rb
@@ -2,6 +2,10 @@ require_relative 'polytreenode'
 
 class KnightPathFinder
 
+    def self.build_move_tree(root_node)
+        
+    end
+
     def initialize(position)
         @root_node = PolyTreeNode.new(position)
     end
