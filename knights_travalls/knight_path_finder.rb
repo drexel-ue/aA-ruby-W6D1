07d0907e4f9386dbb4dead2bbeba5de42a525a28
ikesh pack <packappsforyou@gmail.com>
2019-05-27 23:10:53 -0400

    Converted build_move_tree into an instance method

--- knights_travalls/knight_path_finder.rb
+++ knights_travalls/knight_path_finder.rb
@@ -2,10 +2,6 @@ require_relative 'polytreenode'
 
 class KnightPathFinder
 
-    def self.build_move_tree(root_node)
-
-    end
-
     def self.valid_moves(position)
         x, y = position
         [[x, y + 1], [x + 1, y + 1], [x + 1, y], [x + 1, y - 1], [x, y - 1], [x - 1, y -1], [x - 1, y], [x - 1, y + 1]]
@@ -20,6 +16,10 @@ class KnightPathFinder
         valid_positions = KnightPathFinder.valid_moves(position).select { |pos| !@considered_positions.include?(pos) }
         @considered_positions += valid_positions
         valid_positions
+    end
+
+    def build_move_tree(root_node)
+
     end
 
 end
